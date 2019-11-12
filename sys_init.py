706a75b374ab678295ca125cdc49ae1c3bc60207
ElliotTrapp <trapp.elliot@gmail.com>
2019-11-11 21:57:51 -0800

    Updated linux setup

--- sys_init.py
+++ sys_init.py
@@ -1,5 +1,15 @@
+import logging, argparse, os
 from sys import platform
+from init_linux import init_linux
+from init_macos import init_macos
+from init_windows import init_windows
 
 
-if platform == "linux" or platform == "linux2": # linux elif platform == "darwin": # OS X elif platform == "win32": # Windows.
-
+if platform == "linux" or platform == "linux2": # linux
+    init_linux()
+elif platform == "darwin": # OS X
+    init_macos()
+elif platform == "win32": # Windows.
+    init_windows()
+else:
+    logger.error('ERROR: Failed to determine what platform to initialize!')
