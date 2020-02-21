6b7c032516db62163400d7fe8586a973bc326c47
Elliot Trapp <elliot.trapp@jpl.nasa.gov>
2020-02-21 09:54:06 -0800

    daily crontab backup Fri Feb 21 09:54:06 PST 2020

---
+++ macos/home_dir/cronjobs/git_auto_backup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+PATH=/Users/etrapp/sys_config/macos/home_dir/envs/py3_base/bin:/usr/local/bin:/usr/bin:/Users/etrapp/bin:/Users/etrapp/anaconda3/bin:/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Users/etrapp/anaconda3/envs/py3/bin:/Users/etrapp/anaconda3/condabin:/home/trapp/anaconda3/bin:/Users/etrapp/bin:/Users/etrapp/anaconda3/bin:/bin/
+# Add changes to git
+cd /Users/etrapp/sys_config && /usr/local/bin/git commit -a -m "daily crontab backup `date`"
+
+# Push to remote
+cd /Users/etrapp/sys_config && /usr/local/bin/git push origin master
+
