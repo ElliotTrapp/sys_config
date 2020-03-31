974689a152eaaf301a791fe674ac2fc0a9dc3c8e
Elliot Trapp <elliot.trapp@jpl.nasa.gov>
2020-03-31 10:31:24 -0700

    Added modified PyInquirer, vimrc, and tmux.conf

--- utils/todo/todo.py
+++ utils/todo/todo.py
@@ -15,29 +15,33 @@ def todo(project=None, todo=None):
     if project is None:
         notes_path = os.getenv('notes')
     else:
-        notes_path = os.path.join(os.getenv(project), 'notes/')
+        notes_path = str(os.path.join(os.getenv(project), 'notes/'))
     if todo is None:
         print(horizontalline)
         matches = get_todos(notes_path)
         print_matches(matches)
     else:
         now = datetime.datetime.now()
-        note_dir=os.path.join(os.getenv('notes_path', now.year, now.month))
-        note_path=os.path.join(note_dir, '{}.md'.format(now.day))
+        year = now.strftime('%Y')
+        month = now.strftime('%m')
+        day = now.strftime('%d')
+        note_dir=os.path.join(notes_path, year, month)
+        note_path='{}.md'.format(os.path.join(note_dir, day))
+        print(horizontalline)
         if not os.path.isfile(note_path):
             os.makedirs(note_dir)
             open(note_path, 'a').close()
-            print('{}-{}-{}\n---\n\n > {}'.format(now.day, now.month, now.year, note_path))
+            print('{}-{}-{}\n---\n\n > {}'.format(year, month, day, note_path))
             print('Created new note: {}\n'.format(note_path))
         else:
             print('Note {} already exists. Saving changes to existing file.\n'.format(note_path))
         with open(note_path, 'a') as note:
-            note.write(todo)
+            note.write('#TODO[ ]: {}'.format(todo))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='manage todo lists')
     parser.add_argument('--project', required=False)
-    parser.add_argument('--todo', required=False)
+    parser.add_argument('todo', nargs='?', default=None)
     args = parser.parse_args()
-    todo(project=args.project, todo=args.todo)
-
+    #todo(project=args.project, todo=str(sys.argv[1:]))#todo=args.todo)
+    todo(project=args.project, todo=args.todo)#todo=args.todo)
